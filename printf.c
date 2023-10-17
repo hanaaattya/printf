d54bc9514a9c9dc891a4ae0ae1495f077e04bb14
Reham <r_nour30@yahoo.com>
2023-10-17 16:06:19 -0700

    last

--- printf.c
+++ printf.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * _printf - to print all types of inputs.
+ * _printf - to print all types of inputs like printf.
  * @format: string of all kinds of formating.
- * Return: number of all printed bytes.
+ * * Return: number of all printed bytes.
 */
 int _printf(const char *format, ...)
 {
@@ -11,7 +10,7 @@ int _printf(const char *format, ...)
 	int count = 0;
 	char buffer[20];
 	va_list args;
-
+	
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
@@ -36,26 +35,26 @@ int _printf(const char *format, ...)
 						}
 						if (str == NULL)
 						{
-							count = count + write(1, "(null)", 6);
+							count = count + write(1, "(null)", 6); 
 						}
 					}
 						break;
 				case '%':
 						count = count + newputchar('%');
-					break;
+						break;
 				case 'd':
 				case 'i':
 					{
 						int number = va_arg(args, int);
 						int n = snprintf(buffer, sizeof(buffer), "%d", number);
 						count = count + write(1, buffer, n);
-					break;
+						break;
 					}
 				default:
 					newputchar('%');
 					newputchar(*cpy);
 					count = count + 2;
-						break;
+					break;
 		}
 		}
 		else
