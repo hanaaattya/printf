b7c11e3369982655c857dc378e04d8b5cb37307c
Reham <r_nour30@yahoo.com>
2023-10-17 15:04:23 -0700

    mm

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+int newputchar(char c);
+int _printf(const char *format, ...);
+#endif
